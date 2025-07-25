205645fc26930ebb635d569c85a1c8d75d266b7b
SidParulekar <76064146+SidParulekar@users.noreply.github.com>
2025-07-25 15:14:13 +0530

    Implemented Stacking of items in Inventory
    Made changes to the InventoryItem C# script. Added variable to store item count in inventory. Defined function to update the text in UI.
    
    Made changes in InventoryManager C# script. Added for loop to implement stacking of items in inventory.

--- Assets/Scripts/InventoryItem.cs
+++ Assets/Scripts/InventoryItem.cs
@@ -1,18 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
 public class InventoryItem : MonoBehaviour
 {
-    [HideInInspector] private Item item;
-
+    [HideInInspector] public Item item;
+    [HideInInspector] public int count = 1;
     [SerializeField] private Image image;
+    [SerializeField] private TextMeshProUGUI countText;
 
     public void InitializeItem(Item newItem)
     {
         item = newItem;
         image.sprite = newItem.image;
+        RefreshCount();
+    }
+
+    public void RefreshCount()
+    {
+        countText.text = count.ToString();
     }
 }
--- Assets/Scripts/InventoryManager.cs
+++ Assets/Scripts/InventoryManager.cs
@@ -4,13 +4,29 @@ using UnityEngine;
 
 public class InventoryManager : MonoBehaviour
 {
+    public int maxSlotSpace = 5;
+
     public InventorySlot[] inventorySlots;
 
     public GameObject inventoryItemPrefab;
 
     public bool AddItem(Item item)
     {
-        for(int i=0; i<inventorySlots.Length; i++) 
+        for (int i = 0; i < inventorySlots.Length; i++) // For stacking
+        {
+            InventorySlot slot = inventorySlots[i];
+            InventoryItem itemInSlot = slot.GetComponentInChildren<InventoryItem>();
+
+            if (itemInSlot != null && itemInSlot.item == item && itemInSlot.count < maxSlotSpace && itemInSlot.item.stackable == true)
+            {
+                itemInSlot.count++;
+                itemInSlot.RefreshCount();
+                return true;
+            }
+
+        }
+
+        for (int i=0; i<inventorySlots.Length; i++) // For finding free slots
         { 
             InventorySlot slot = inventorySlots[i]; 
             InventoryItem itemInSlot = slot.GetComponentInChildren<InventoryItem>();
@@ -20,6 +36,7 @@ public class InventoryManager : MonoBehaviour
                 SpawnNewItem(item, slot);
                 return true;
             }
+
         }
 
         return false;
